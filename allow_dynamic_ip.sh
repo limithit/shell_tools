b24df175db49c985004f6c2d8883173b6e6e9822
Gandalf <zhibu1991@gmail.com>
2019-04-30 13:02:38 +0800

    Update allow_dynamic_ip.sh

--- allow_dynamic_ip.sh
+++ allow_dynamic_ip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #*/30 * * * * root /bin/bash /root/allow.sh  >> /dev/null 2>&1
 ip=`curl ifconfig.me`
-ipold=`ssh -o "ProxyCommand ssh -p 2288 root@115.236.xx.xx nc -w 1800 %h %p" -p22 115.238.xx.xx 'cat /etc/nginx/allowip.conf'`
+ipold=`ssh -o "ProxyCommand ssh -p 22 root@115.xx.xx.xx nc -w 1800 %h %p" -p22 116.xx.xx.xx 'cat /etc/nginx/allowip.conf'`
 echo $ipold
 if [ "allow $ip;" = "$ipold" ]
 then
@@ -10,5 +10,5 @@ elif [ "allow $ip;" != "$ipold" ]
 then
 	echo "allow $ip;" >allowip.conf
 fi
-scp -o "ProxyCommand ssh -p 2288 root@115.236.xx.xx nc -w 1800 %h %p" allowip.conf   115.238.xx.xx:/etc/nginx/  
-ssh -o "ProxyCommand ssh -p 2288 root@115.236.xx.xx nc -w 1800 %h %p" -p22 115.238.xx.xx '/etc/init.d/nginx reload'
+scp -o "ProxyCommand ssh -p 22 root@115.xx.xx.xx nc -w 1800 %h %p" allowip.conf   116.xx.xx.xx:/etc/nginx/  
+ssh -o "ProxyCommand ssh -p 22 root@115.xx.xx.xx nc -w 1800 %h %p" -p22 115.238.xx.xx '/etc/init.d/nginx reload'
