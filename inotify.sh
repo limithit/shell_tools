daa840a8ba4c38ee6220d7bc8efcd65c0fca8fb3
Gandalf <zhibu1991@gmail.com>
2019-11-22 12:05:03 +0800

    Update inotify.sh

--- inotify.sh
+++ inotify.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
-while inotifywait -e modify /var/log/nginx/access.log; do
-        if tail -n 1 /var/log/nginx/access.log | grep spider; then
-                echo "It appears !"
-        fi
+#! /bin/sh
+# code by 西门吹雪
+while inotifywait -e modify /var/log/secure; do
+cat /var/log/secure |awk '/Failed/{print $(NF-3)}'|sort|uniq -c|awk '{print $2"="$1;}' > /root/black.txt 
+DEFINE="10" 
+for i in `cat  /root/black.txt` 
+do
+         IP=`echo $i |awk -F= '{print $1}'` 
+        NUM=`echo $i|awk -F= '{print $2}'` 
+        if [ $NUM -gt $DEFINE ]; 
+        then 
+         grep $IP /etc/hosts.deny > /dev/null 
+          if [ $? -gt 0 ]; 
+          then 
+          echo "sshd:$IP" >> /etc/hosts.deny 
+          fi 
+        fi 
 done
+done
+
