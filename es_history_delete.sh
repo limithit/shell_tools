c889b97ed43088c85b52d437874b5d4864c8e7f1
Gandalf <zhibu1991@gmail.com>
2020-01-04 10:29:58 +0800

    Add files via upload

---
+++ es_history_delete.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -x
+if [ $# = 2 ]; then
+
+for ((i=1; i<=9; i++))
+ do
+curl -H "Content-Type: application/json" -XDELETE 'http://localhost:9200/zipkin:span-'$1'-'$2'-0'$i'' -d '{
+"query" : { 
+        "match_all" : {}
+}
+}'
+done
+
+for ((i=10; i<=31; i++)) 
+ do
+curl -H "Content-Type: application/json" -XDELETE 'http://localhost:9200/zipkin:span-'$1'-'$2'-'$i'' -d '{
+"query" : { 
+	"match_all" : {}
+}
+}'
+done
+else 
+  echo "Example: es.sh 2019 12"
+   exit 1
+fi
