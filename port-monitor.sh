3965ee1560d6da57a193c47e90e9e8efe5aa6bc7
Gandalf <zhibu1991@gmail.com>
2020-09-29 09:46:17 +0800

    Update port-monitor.sh

--- port-monitor.sh
+++ port-monitor.sh
@@ -10,7 +10,7 @@ do
 sleep 3
 datetime=`date '+%Y-%m-%d %H:%M:%S'`
  port=`netstat -tupln|grep -i 8899|awk '{print $4}'|cut -d : -f 2|awk '{print $1}'`
-if [ "$port" == "8899" ]
+if [ $port -eq 8899 ]
 then
         continue
 else
