5d93d34634620623e9dbee9c9491653ae9e280f9
Gandalf <zhibu1991@gmail.com>
2023-05-29 18:06:45 +0800

    Update metadata_cvs.sh

--- metadata_cvs.sh
+++ metadata_cvs.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 set -x
 # Convert preprocessed image text links together to metadata.csv 
-# Create an image dataset  
-# https://huggingface.co/docs/datasets/v2.12.0/en/image_dataset
-#cd /root/img
+#cd /root/ppmt
  cd $1
 if [ $# = 1 ]; then
- echo "file_name,text" >> metadata.csv
 for file in *.png; do
     filename=$(basename "$file")
     filename="${filename%.*}"
     txt_file="$filename.txt"
     if [ -f "$txt_file" ]; then
         content=$(cat "$txt_file")
-        echo "$filename.png,$content" >> metadata.csv
+        echo "{\"file_name\":\"$filename.png\",\"text\":\"$content\""} >> metadata.jsonl
     fi
 done
-else 
-  echo "Example: metadata_cvs.sh /path/to/image_txt"
+else
+  echo "Example: es.sh path/to/image_txt"
    exit 1
 fi
